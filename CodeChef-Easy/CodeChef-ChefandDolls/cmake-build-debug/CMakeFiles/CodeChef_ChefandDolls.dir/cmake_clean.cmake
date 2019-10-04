23f0ab4c499e67ce08fa21d66ff37497de7d57e2
Mayan Bhadage <mayanbhadage@Mayans-MacBook-Pro.local>
2019-10-04 18:42:12 -0400

    practice

---
+++ CodeChef-Easy/CodeChef-ChefandDolls/cmake-build-debug/CMakeFiles/CodeChef_ChefandDolls.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CodeChef_ChefandDolls.dir/main.cpp.o"
+  "CodeChef_ChefandDolls.pdb"
+  "CodeChef_ChefandDolls"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CodeChef_ChefandDolls.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CodeChef-Easy/CodeChef-ChefandDolls/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+struct Doll
+{
+    int dollNum;
+    int index;
+    
+
+};
+
+
+int main() {
+    int numLines = 0;
+    std::cin>>numLines;
+
+    while (numLines--)
+    {
+        int size;
+        int result;
+        std::cin>>size;
+
+        std::vector<Doll> dolls (size);
+
+
+
+
+        for(int i = 0; i < size; i++)
+        {
+            int num;
+            std::cin>>num;
+
+            dolls[i].dollNum = num;
+
+
+        }
+
+        for (int i = 0; i < dolls.size(); i++)
+        {
+            for (int j = 0; j < dolls.size(); j++)
+            {
+                if(i == j)
+                {
+                    continue;
+                }
+                else
+                {
+                    if(dolls[i].dollNum == dolls[j].dollNum)
+                    {
+                        dolls.erase(dolls.begin() + i);
+                        dolls.erase(dolls.begin()+ (j-1));
+                        i=0;
+                        j=0;
+                    }
+                }
+
+                if(dolls.size()==1)
+                {
+                    result = dolls[0].dollNum;
+                }
+            }
+
+
+        }
+
+        std::cout<<result<<std::endl;
+
+    }
+
+
+    return 0;
+}
